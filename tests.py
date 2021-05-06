9eb01d2ee57d76343e67c11ef36b12f91669563d
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-05-06 15:37:00 +0300

    Added tests

---
+++ tests.py
@@ -0,0 +1,20 @@
+import pytest
+
+import main as tested_app
+
+
+@pytest.fixture
+def client():
+    tested_app.app.config['TESTING'] = True
+    app = tested_app.app.test_client()
+    return app
+
+
+def test_get(client):
+    r = client.get('/')
+    assert r.data.decode('utf-8') == 'У меня получилось!'
+
+
+def test_post(client):
+    r = client.post('/')
+    assert r.status_code == 405
